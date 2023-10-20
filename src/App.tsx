0b07712b92416bae705fbd298ad4b0727f65aaa3
Matheus Andrade <matheusas301@gmail.com>
2023-10-20 17:00:48 -0300

    modificando-layout-header

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { SolucaoPorto } from './pages/SolucaoPorto/SolucaoPorto'
-import { Organograma } from './pages/Organograma/Organograma';
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Organograma/>
+    <SolucaoPorto/>
   );
 }
 
