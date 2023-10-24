b469c81db1fcf102c1ed5cd3fa3ce0d6c8e87cce
Danilo Mendonça <danilomendonca08@gmail.com>
2023-10-24 16:54:44 -0300

    Criado componente de imagem para melhorias do projeto

---
+++ src/components/Imagem/Imagem.style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledImg = styled.img`
+    max-width: 100%;
+
+    @media (min-width: 768px) {
+        width: 70vw;
+    }
+
+    @media (min-width: 1920px) {
+        height: 45vh;
+    }
+`
