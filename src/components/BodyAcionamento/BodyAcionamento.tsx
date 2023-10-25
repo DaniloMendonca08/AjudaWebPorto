6e32d500c458bf4aaec7dd1acd5235d9f6afe6ea
Danilo Mendonça <danilomendonca08@gmail.com>
2023-10-25 14:57:25 -0300

    Ajustes no BodyAcionamento.tsx e importação dos componentes de titulo, paragrafo e imagem

--- src/components/BodyAcionamento/BodyAcionamento.tsx
+++ src/components/BodyAcionamento/BodyAcionamento.tsx
@@ -1,27 +1,21 @@
-import { StyledH1BodyAcionamento } from "./BodyAcionamento.style"
+import { Imagem } from "../Imagem/Imagem"
+import { Paragrafo } from "../Paragrafo/Paragrafo"
+import { Titulo } from "../Titulo/Titulo"
 import { StyledBodyAcionamento } from "./BodyAcionamento.style"
-import { StyledImgBodyAcionamento } from "./BodyAcionamento.style"
 import { StyledDivBodyAcionamento } from "./BodyAcionamento.style"
-import { StyledTagPBodyAcionamento } from "./BodyAcionamento.style"
+import imgAcionamentoSinistro from "../../assets/imgAcionamentoSinistro.svg"
 
-interface BodyAcionamentoprops{
-    titulo: string,
-    img: string,
-    texto: string,
-    descricao?: string,
-}
+export const paragrafoAcionamento = "Nesta parte, introduziremos algumas perguntas que são muito difíceis de serem indentificadas, seram poucas perguntas e bem objetivas, até porque neste momento o assegurado não gostaria de ter que responder uma série muito grande de questionamentos, mas, por trás destas simples perguntas, estará sendo trabalhado uma grande base de dados que fará com que a melhor escolha seja feita."
 
-export const BodyAcionamento = ({titulo, img, texto, descricao}: BodyAcionamentoprops) => {
+export const BodyAcionamento = () => {
 return(
         <>
         <StyledBodyAcionamento>
 
             <StyledDivBodyAcionamento>
-                <StyledH1BodyAcionamento> {titulo} </StyledH1BodyAcionamento>
-
-                <StyledImgBodyAcionamento  src={img} alt={descricao} />
-
-                <StyledTagPBodyAcionamento>{texto}</StyledTagPBodyAcionamento>
+            <Titulo titulo={"Acionamento de Sinistro"} />
+            <Imagem img={imgAcionamentoSinistro} descricao= "Imagem de uma pessoa ligando para algúem/algum lugar"/>
+            <Paragrafo texto={paragrafoAcionamento}/>
             </StyledDivBodyAcionamento>
 
         </StyledBodyAcionamento>
