e50e9212ed35050fdd38f1f0b2ca5afd7eb0c303
Danilo Mendonça <danilomendonca08@gmail.com>
2023-10-26 15:37:50 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { AcionamentoSinistro } from './pages/AcionamentoSinistro/AcionamentoSinistro';
+import { CentralSolucoes } from './pages/CentralSolucoes/CentralSolucoes';
 import { SolucaoPorto } from './pages/SolucaoPorto/SolucaoPorto'
 
 
 function App() {
   return (
-    <AcionamentoSinistro/>
+    <CentralSolucoes/>
   );
 }
 
