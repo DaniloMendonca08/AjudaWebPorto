f1f0fa4194666d732e22d8344ebe93aa316616fe
Guilherme <guiromerolima08@hotmail.com>
2023-10-26 15:50:16 -0300

    Deletado styleds consts que foram utilizadas para H1 e paragrafo

--- src/components/BodyPagPrincipal/BodyPagPrincipal.style.ts
+++ src/components/BodyPagPrincipal/BodyPagPrincipal.style.ts
@@ -12,63 +12,6 @@ export const StyledDivPagPrincipal = styled.div`
 
 `
 
-export const StyledH1PagPrincipal = styled.h1`
-    font-family: 'Open Sans';
-    font-size: 32px;
-    font-weight: 300;
-    padding: 15px 0;
-
-@media (min-width: 320px) {
-    font-size: 22px;
-}
-
-@media (min-width: 1024px) {
-    font-size: 30px;
-}
-
-@media (min-width:1440px) {
-    font-size: 35px;
-}
-
-@media (min-width: 1920px) {
-    font-size: 30px;  
-} 
-`
-
-export const StyledParagraphPagPrincipal = styled.p`
-    font-family: 'Open Sans';
-    font-size: 20px;
-    font-weight: 300;
-    padding-top: 0px;
-    padding-bottom: 15px;
-    width: 90%;
-
-@media (min-width: 320px) {
-    font-size: 11px;
-} 
-
-@media (min-width: 425px) {
-    font-size: 12px;
-}
-
-@media (min-width: 768px) {
-    font-size: 14px;
-}
-
-@media (min-width: 1024px) {
-    font-size: 19px;
-}
-
-@media (min-width: 1440px) {
-    font-size: 22px;
-}
-
-@media (min-width: 1920px) {
-    font-size: 17px;
-}
-
-`
-
 export const StyledIframePagPrincipal = styled.iframe`
     height: 20vh;
 
