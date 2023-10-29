9ffa7f34205c8177a46a2bd51137b07c0ff35015
Danilo Mendonça <danilomendonca08@gmail.com>
2023-10-29 20:32:26 -0300

    Apagado paragrafo que não esta sendo utilizado

--- src/pages/AcionamentoSinistro/AcionamentoSinistro.tsx
+++ src/pages/AcionamentoSinistro/AcionamentoSinistro.tsx
@@ -2,8 +2,6 @@ import { Header } from "../../components/Header/Header";
 import { Footer } from "../../components/Footer/Footer";
 import { BodyAcionamento } from "../../components/BodyAcionamento/BodyAcionamento";
 
-export const paragrafoAcionamentoS = "Nesta parte, introduziremos algumas perguntas que são muito difíceis de serem indentificadas, seram poucas perguntas e bem objetivas, até porque neste momento o assegurado não gostaria de ter que responder uma série muito grande de questionamentos, mas, por trás destas simples perguntas, estará sendo trabalhado uma grande base de dados que fará com que a melhor escolha seja feita."
-
 export const AcionamentoSinistro = () => {
     return(
         <>
