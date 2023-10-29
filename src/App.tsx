4ec6f7d6c7e8c1024ee91cd292ccf06301409f98
Danilo Mendonça <danilomendonca08@gmail.com>
2023-10-29 20:36:29 -0300

    Ajuste no tamanho da imagem e teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { AcionamentoSinistro } from './pages/AcionamentoSinistro/AcionamentoSinistro';
 import { CentralSolucoes } from './pages/CentralSolucoes/CentralSolucoes';
+import { RevisoesBreves } from './pages/RevisoesBreves/RevisoesBreves';
 import { SolucaoPorto } from './pages/SolucaoPorto/SolucaoPorto'
 
 
 function App() {
   return (
-    <CentralSolucoes/>
+    <RevisoesBreves/>
   );
 }
 
--- src/components/Imagem/Imagem.style.ts
+++ src/components/Imagem/Imagem.style.ts
@@ -2,11 +2,8 @@ import styled from "styled-components";
 
 export const StyledImg = styled.img`
     max-width: 100%;
-
-    @media (min-width: 768px) {
-        width: 70vw;
-    }
-
+    width: 80vw;
+  
     @media (min-width: 1920px) {
         height: 45vh;
     }
